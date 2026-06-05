6b566fc3d2f1d84cd683d87e9acbaa5e70cf2ac2
SeokTaeJun-Fox <moderneuropean81@gmail.com>
2026-06-05 18:06:47 +0900

    feat:

---
+++ src/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.infrastructure.oracle import engine, Base
+from app.infrastructure.redis import start_redis_client
+from app.security.cors import setup_cors
+from app.security.security_headers import setup_security
+from app.apis import member_api, auth_api, file_api
+
+import app.models
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        print("Oracle DB 연결 성공!")
+
+    async with start_redis_client(app):
+        print("Redis DB 연결 성공!")
+
+    yield
+
+# Swagger
+# http://localhost:8000/docs
+app = FastAPI(
+    lifespan=lifespan,
+    title="fast API Backend",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
+
+setup_cors(app)
+setup_security(app)
+
+app.include_router(member_api.router, prefix="/members", tags=["members"])
+app.include_router(auth_api.router, prefix="/auth", tags=["/auth"])
+app.include_router(file_api.router, prefix="/files", tags=["/files"])
+
